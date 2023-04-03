67c961cb75985240626e00e80a4976344cd3ff2e
Jrox06 <joelgarciaban@gmail.com>
2023-04-03 13:48:20 -0400

    MOD| unit test

--- src/test/java/com/evaluacion/crud/services/UserServiceTest.java
+++ src/test/java/com/evaluacion/crud/services/UserServiceTest.java
@@ -1,29 +1,41 @@
 package com.evaluacion.crud.services;
 
+import com.evaluacion.crud.dto.PhoneDto;
+import com.evaluacion.crud.dto.UserDto;
+import com.evaluacion.crud.dto.UserResponseDto;
 import com.evaluacion.crud.entity.User;
+import com.evaluacion.crud.execeptions.ExceptionRuntimeHandler;
 import com.evaluacion.crud.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-
-@ExtendWith(MockitoExtension.class)
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class UserServiceTest {
 
+   @Autowired
+   private UserRepository userRepository;
+
     @Autowired
-    private UserRepository userRepository;
+    private UserServiceImpl userService;
+
     User userRequest = new User();
+    UserDto userDto = new UserDto();
+    List<PhoneDto> phones = new ArrayList<>();
+    PhoneDto phone = new PhoneDto();
+
+
 
     @BeforeEach
     void setUp() {
@@ -34,31 +46,54 @@ public class UserServiceTest {
         userRequest.setCreateAt(LocalDateTime.now());
         userRequest.setIsActive(true);
         userRequest.setPassword("Hunter837");
+
+        phone.setCitycode("1");
+        phone.setNumber("1234123123");
+        phone.setContrycode("58");
+        phones.add(phone);
+        userDto.setName("Joel");
+        userDto.setPassword("Hunter837");
+        userDto.setPhones(phones);
+
     }
 
     @Test
     @DisplayName("Successful user saving")
-    void saveTest() {
-        User user = userRepository.save(userRequest);
-
-        assertThat(user.getId()).isNotEqualTo(null);
+    void saveUserHappyTest() {
+        userDto.setEmail("joel@gmail.com");
+        UserResponseDto user = userService.createUser(userDto);
+        Assertions.assertNotNull(user);
     }
 
     @Test
-    @DisplayName("Successful user email search")
-    void findEmailTest() {
-        User user = userRepository.save(userRequest);
-
-        User userResponse = userRepository.findById(user.getId()).get();
-        Assertions.assertNotNull(userResponse);
+    @DisplayName("Email Error: user saving")
+    void errorSaveTest() {
+        try {
+            userDto.setEmail("juan@rodrigues.org");
+            userService.createUser(userDto);
+            userService.createUser(userDto);
+        } catch (ExceptionRuntimeHandler e) {
+            Assertions.assertEquals("El correo ya se encuentra registrado", e.getMessage());
+        }
     }
 
+    void errorEmailTest() {
+        try {
+            userDto.setEmail("joel@a-com");
+            userService.createUser(userDto);
+            userService.createUser(userDto);
+        } catch (ExceptionRuntimeHandler e) {
+            Assertions.assertEquals("El email no tiene un formato correcto", e.getMessage());
+        }
+    }
     @Test
-    @DisplayName("Successful user search")
-    void findUserId() {
-        User user = userRepository.save(userRequest);
-
-        Optional<User> userResponse = userRepository.findById(user.getId());
-        Assertions.assertNotNull(userResponse);
+    @DisplayName("Error Password: user saving")
+    void errorPasswordTest() {
+        try {
+            userDto.setPassword("123");
+            userService.createUser(userDto);
+        } catch (ExceptionRuntimeHandler e) {
+            Assertions.assertEquals("Password no cumple con las condiciones", e.getMessage());
+        }
     }
 }
